a15effb316f24a710f739c87b0b987f41eeb6770
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-26 05:23:25 +0700

    perbaikan menghilangkan nama afif

--- backend/app/models/types.py
+++ backend/app/models/types.py
@@ -12,7 +12,6 @@ class AccountParent(Base, AuditMixin):
     __tablename__ = 'account_parents'
     
     id = Column(Integer, primary_key=True)
-    # afif
     account_no = Column(Numeric, nullable=False, unique=True, index=True)
     name = Column(String, nullable=True)
     account_type = Column(String, nullable=True)
